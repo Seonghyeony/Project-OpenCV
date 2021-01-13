37150da69615139e26d5dd6846395ec1f9eefefe
Seonghyeony <sunghyun7949@naver.com>
2021-01-13 15:28:09 +0900

    Update: mask_op, copyTo, crop

--- OpenCV-basic/ch02/mask_op.py
+++ OpenCV-basic/ch02/mask_op.py
@@ -1,7 +1,6 @@
 import sys
 import cv2
 
-
 # 마스크 영상을 이용한 영상 합성
 src = cv2.imread('airplane.bmp', cv2.IMREAD_COLOR)
 mask = cv2.imread('mask_plane.bmp', cv2.IMREAD_GRAYSCALE)
@@ -11,33 +10,39 @@ if src is None or mask is None or dst is None:
     print('Image load failed!')
     sys.exit()
 
-cv2.copyTo(src, mask, dst)
-# dst[mask > 0] = src[mask > 0]
+'''
+src, mask, dst = 사이즈 같아야 함.
+src, dst = 타입이 같아야 함.
+mask는 무조건 grayscale 이어야 함.
+'''
+# cv2.copyTo(src, mask, dst)
 
-cv2.imshow('src', src)
-cv2.imshow('dst', dst)
-cv2.imshow('mask', mask)
+dst[mask > 0] = src[mask > 0]   # boolean 값. 이건 참조 형태.
+
+# cv2.imshow('src', src)
+# cv2.imshow('dst', dst)
+# cv2.imshow('mask', mask)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-# 알파 채널을 마스크 영상으로 이용
-src = cv2.imread('cat.bmp', cv2.IMREAD_COLOR)
-logo = cv2.imread('opencv-logo-white.png', cv2.IMREAD_UNCHANGED)
+'''------------------------------------------------------------------------------------'''
+# 4채널 중 알파 채널을 마스크 영상으로 이용
+src = cv2.imread('opencv-logo-white.png', cv2.IMREAD_UNCHANGED)    # UNCHANGED를 써야함. 4채널
+mask = src[:, :, -1]
+src = src[:, :, :3]
+dst = cv2.imread('field.bmp', cv2.IMREAD_COLOR)
 
-if src is None or logo is None:
-    print('Image load failed!')
-    sys.exit()
+h, w = src.shape[:2]
 
-mask = logo[:, :, 3]    # mask는 알파 채널로 만든 마스크 영상
-logo = logo[:, :, :-1]  # logo는 b, g, r 3채널로 구성된 컬러 영상
-h, w = mask.shape[:2]
-crop = src[10:10+h, 10:10+w]  # logo, mask와 같은 크기의 부분 영상 추출
+# dst의 일부분을 잘라와서 공유한다.
+crop = dst[10:h+10, 10:w+10]
 
-cv2.copyTo(logo, mask, crop)
-#crop[mask > 0] = logo[mask > 0]
+cv2.copyTo(src, mask, crop)
+
+# dst[mask > 0] = src[mask > 0]   # boolean 값. 이건 참조 형태.
 
 cv2.imshow('src', src)
-cv2.imshow('logo', logo)
+cv2.imshow('dst', dst)
 cv2.imshow('mask', mask)
 cv2.waitKey()
 cv2.destroyAllWindows()
