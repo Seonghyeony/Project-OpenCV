dddbacd0c8555292482d94b5747a6c23d3edb0c2
Seonghyeony <sunghyun7949@naver.com>
2021-01-13 22:22:26 +0900

    Update: video write, fourcc

--- OpenCV-basic/ch02/video_out.py
+++ OpenCV-basic/ch02/video_out.py
@@ -8,12 +8,14 @@ if not cap.isOpened():
     print("Camera open failed!")
     sys.exit()
 
+# cap.get() 이 Double형이라서 정수 값 만들기 위해 round
 w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
+# fps = cap.get(cv2.CAP_PROP_FPS) # 카메라마다 다름.
+fps = 30
 
 fourcc = cv2.VideoWriter_fourcc(*'DIVX') # *'DIVX' == 'D', 'I', 'V', 'X'
-delay = round(1000 / fps)
+delay = round(1000 / fps) # 한 프레임과 그 다음 프레임의 시간 간격
 
 out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
 
@@ -27,13 +29,17 @@ while True:
 
     if not ret:
         break
+    
+    # inversed = ~frame
+    edge = cv2.Canny(frame, 50, 150) # grayscale이다.
+    # grayscale을 저장하려면 color로 변환을 해주어야 한다.
+    edge_color = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
 
-    inversed = ~frame
-
-    out.write(inversed)
+    out.write(edge_color)
 
     cv2.imshow('frame', frame)
-    cv2.imshow('inversed', inversed)
+    cv2.imshow('edge', edge)
+    cv2.imshow('edge_color', edge_color)
 
     if cv2.waitKey(delay) == 27:
         break
