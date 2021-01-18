f4bd02407ee3a9d00d3923a4a65615d58f088f16
Seonghyeony <sunghyun7949@naver.com>
2021-01-19 00:47:27 +0900

    Feat: Color space(BGR, HSV, YCrCb)

--- OpenCV-basic/ch03/color.py
+++ OpenCV-basic/ch03/color.py
@@ -14,17 +14,18 @@ if src is None:
 print('src.shape:', src.shape)  # src.shape: (480, 640, 3)
 print('src.dtype:', src.dtype)  # src.dtype: uint8
 
-# RGB 색 평면 분할
-b_plane, g_plane, r_plane = cv2.split(src)
-
-#b_plane = src[:, :, 0]
-#g_plane = src[:, :, 1]
-#r_plane = src[:, :, 2]
-
+# hsv color space
+# CV_8U 에서는 H 색 공간은 0 ~ 179로 표현.
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+planes = cv2.split(src_hsv)
+ 
 cv2.imshow('src', src)
-cv2.imshow('B_plane', b_plane)
-cv2.imshow('G_plane', g_plane)
-cv2.imshow('R_plane', r_plane)
+cv2.imshow('planes[0]', planes[0])
+cv2.imshow('planes[1]', planes[1])
+cv2.imshow('planes[2]', planes[2])
+# cv2.imshow('B_plane', b_plane)
+# cv2.imshow('G_plane', g_plane)
+# cv2.imshow('R_plane', r_plane)
 cv2.waitKey()
 
 cv2.destroyAllWindows()
