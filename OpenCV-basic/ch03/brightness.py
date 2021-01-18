4b200fe5fd8ea542e00a3378701841f4bf4e0e14
Seonghyeony <sunghyun7949@naver.com>
2021-01-18 21:28:15 +0900

    Feat: Brightness control

--- OpenCV-basic/ch03/brightness.py
+++ OpenCV-basic/ch03/brightness.py
@@ -4,20 +4,7 @@ import cv2
 
 
 # 그레이스케일 영상 불러오기
-src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
-
-if src is None:
-    print('Image load failed!')
-    sys.exit()
-
-dst = cv2.add(src, 100)
-#dst = np.clip(src + 100., 0, 255).astype(np.uint8)
-
-cv2.imshow('src', src)
-cv2.imshow('dst', dst)
-cv2.waitKey()
-
-# 컬러 영상 불러오기
+# src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE) # uint8
 src = cv2.imread('lenna.bmp')
 
 if src is None:
@@ -25,7 +12,7 @@ if src is None:
     sys.exit()
 
 dst = cv2.add(src, (100, 100, 100, 0))
-#dst = np.clip(src + 100., 0, 255).astype(np.uint8)
+# dst = np.clip(src + 100., 0, 255).astype(np.uint8) # src: ndarray type, 255보다 커지면 0부터 셋팅 한다.
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
