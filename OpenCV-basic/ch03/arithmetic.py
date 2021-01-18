b954bf7fb8ae44aa057080f442be696d79e916f6
Seonghyeony <sunghyun7949@naver.com>
2021-01-18 21:48:03 +0900

    Feat: Arithmetic and logical operations

--- OpenCV-basic/ch03/arithmetic.py
+++ OpenCV-basic/ch03/arithmetic.py
@@ -11,11 +11,12 @@ if src1 is None or src2 is None:
     print('Image load failed!')
     sys.exit()
 
-dst1 = cv2.add(src1, src2, dtype=cv2.CV_8U)
-dst2 = cv2.addWeighted(src1, 0.5, src2, 0.5, 0.0)
-dst3 = cv2.subtract(src1, src2)
-dst4 = cv2.absdiff(src1, src2)
+dst1 = cv2.add(src1, src2, dtype=cv2.CV_8U) # 덧셈 연산 
+dst2 = cv2.addWeighted(src1, 0.5, src2, 0.5, 0.0)   # 가중치 연산
+dst3 = cv2.subtract(src1, src2) # 뺄셈 연산
+dst4 = cv2.absdiff(src1, src2)  # 차 연산
 
+# 2행 3열 1번 째, 2행 3열 2번 째....
 plt.subplot(231), plt.axis('off'), plt.imshow(src1, 'gray'), plt.title('src1')
 plt.subplot(232), plt.axis('off'), plt.imshow(src2, 'gray'), plt.title('src2')
 plt.subplot(233), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('add')
