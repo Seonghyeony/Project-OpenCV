6018a2583856f404b8892c8dfe095583b63fbd1e
Seonghyeony <sunghyun7949@naver.com>
2021-01-19 17:09:22 +0900

    Feat: Extract color using RGB, HSV

--- OpenCV-basic/ch03/inrange1.py
+++ OpenCV-basic/ch03/inrange1.py
@@ -2,18 +2,21 @@ import sys
 import numpy as np
 import cv2
 
+# 밝게 찍힌 영상.
+# src = cv2.imread('candies.png')
 
-src = cv2.imread('candies.png')
-#src = cv2.imread('candies2.png')
+# 어둡게 찍힌 영상에서는 RGB에서 추출이 잘 안된다.
+src = cv2.imread('candies2.png')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
+# hsv 컬러로 변환.
 src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
-dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))
-dst2 = cv2.inRange(src_hsv, (50, 150, 0), (80, 255, 255))
+dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))       # BGR
+dst2 = cv2.inRange(src_hsv, (50, 150, 0), (80, 255, 255))   # HSV
 
 cv2.imshow('src', src)
 cv2.imshow('dst1', dst1)
