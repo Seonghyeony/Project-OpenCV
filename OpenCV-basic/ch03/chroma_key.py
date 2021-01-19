8ca40ec74d9adbcadd432d44ddfaa80ef61a6c8b
Seonghyeony <sunghyun7949@naver.com>
2021-01-19 22:09:13 +0900

    Feat: 크로마 키 합성

--- OpenCV-basic/ch03/chroma_key.py
+++ OpenCV-basic/ch03/chroma_key.py
@@ -6,6 +6,9 @@ import cv2
 # 녹색 배경 동영상
 cap1 = cv2.VideoCapture('woman.mp4')
 
+# 내 장치 카메라
+# cap1 = cv2.VideoCapture(0)
+
 if not cap1.isOpened():
     print('video open failed!')
     sys.exit()
@@ -18,16 +21,25 @@ if not cap2.isOpened():
     sys.exit()
 
 # 두 동영상의 크기, FPS는 같다고 가정
-frame_cnt1 = round(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
+w = round(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = round(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+frame_cnt1 = round(cap1.get(cv2.CAP_PROP_FRAME_COUNT))  # 총 프레임 수
 frame_cnt2 = round(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
+
+print('w x h: {} x {}'.format(w, h))
 print('frame_cnt1:', frame_cnt1)
 print('frame_cnt2:', frame_cnt2)
 
 fps = cap1.get(cv2.CAP_PROP_FPS)
-delay = int(1000 / fps)
+delay = int(1000 / fps) # 두개의 프레임 사이에 들어갈 시간 간격.
+
+# 출력 동영상 객체 생성
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
 
 # 합성 여부 플래그
-do_composit = False
+do_composit = False # true 면 합성을 한다.
 
 # 전체 동영상 재생
 while True:
@@ -42,21 +54,28 @@ while True:
 
         if not ret2:
             break
+        
+        # 장치 카메라를 사용할 경우 영상 크기 변경.
+        frame2 = cv2.resize(frame2, (w, h))
 
         # HSV 색 공간에서 녹색 영역을 검출하여 합성
         hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, (50, 150, 0), (70, 255, 255))
+        # copyTo(): 마스크 연산; 흰색 영역에 대해서만 연산.
         cv2.copyTo(frame2, mask, frame1)
 
+    out.write(frame1)
+
     cv2.imshow('frame', frame1)
-    key = cv2.waitKey(delay)
+    key = cv2.waitKey(delay) # 41ms 기다리고 다음 영상으로.
 
     # 스페이스바를 누르면 do_composit 플래그를 변경
     if key == ord(' '):
         do_composit = not do_composit
-    elif key == 27:
+    elif key == 27:     # ESC를 누르면 종료.
         break
 
 cap1.release()
 cap2.release()
+out.release()
 cv2.destroyAllWindows()
