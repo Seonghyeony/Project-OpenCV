3c08faa01314abaffca8f309e8247305c3df0784
Seonghyeony <sunghyun7949@naver.com>
2021-01-19 21:16:50 +0900

    Feat: Histogram 역투영; calcBackProject

--- OpenCV-basic/ch03/backproj1.py
+++ OpenCV-basic/ch03/backproj1.py
@@ -11,13 +11,16 @@ if src is None:
     print('Image load failed!')
     sys.exit()
 
+# selectROI(): 마우스를 이용하여 어떤 영역을 드래그해서 선택 가능.
 x, y, w, h = cv2.selectROI(src)
 
 src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
+# crop은 사용자가 선택한 사각형 영역의 부분 영상.
 crop = src_ycrcb[y:y+h, x:x+w]
 
-channels = [1, 2]
-cr_bins = 128
+# 0번 채널; Y: 밝기
+channels = [1, 2]   # 0번 채널은 사용하지 않는다. 색상 정보만 사용.
+cr_bins = 128       # 단순화; 256을 써도 달라지진 않는다.
 cb_bins = 128
 histSize = [cr_bins, cb_bins]
 cr_range = [0, 256]
@@ -29,6 +32,8 @@ hist_norm = cv2.normalize(cv2.log(hist+1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV
 
 # 입력 영상 전체에 대해 히스토그램 역투영
 
+# 위에서 계산한 hist 값을 이용해서 
+# calcBackProject()로 내가 원하는 영역만 골라낼 수 있다. 
 backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1)
 dst = cv2.copyTo(src, backproj)
 
--- OpenCV-basic/ch03/backproj2.py
+++ OpenCV-basic/ch03/backproj2.py
@@ -15,7 +15,13 @@ ref_ycrcb = cv2.cvtColor(ref, cv2.COLOR_BGR2YCrCb)
 
 channels = [1, 2]
 ranges = [0, 256, 0, 256]
+
+# calcHist()에서 mask 값을 입력하면 이 mask 영역에서의 히스토그램을 구한다.
 hist = cv2.calcHist([ref_ycrcb], channels, mask, [128, 128], ranges)
+
+# cv2.log 스케일로 만들어 주는 것이 좋다. 작은 것들은 0에 가깝게 나오고 큰 것들만 밝게 나오기 때문.
+# cv2.log(hist + 1): +1 을 하는 이유는 log(hist) 값이 0일 수 있기 때문.
+# grayscale로 변환: cv2.CV_8U
 hist_norm = cv2.normalize(cv2.log(hist + 1), None, 0, 255, 
                           cv2.NORM_MINMAX, cv2.CV_8U)
 
@@ -28,7 +34,8 @@ if src is None:
 
 src_ycrcb = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)
 
-backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1)
+# 아까 계산한 hist를 이용하여.
+backproj = cv2.calcBackProject([src_ycrcb], channels, hist, ranges, 1) # grayscale 영상이다.
 
 cv2.imshow('src', src)
 cv2.imshow('hist_norm', hist_norm)
