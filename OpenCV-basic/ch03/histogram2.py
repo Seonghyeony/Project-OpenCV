a95c4299fac6081c7d5af37ebadfe4e8c7261dd2
Seonghyeony <sunghyun7949@naver.com>
2021-01-19 14:16:03 +0900

    Feat: histogram to opencv func

--- OpenCV-basic/ch03/histogram2.py
+++ OpenCV-basic/ch03/histogram2.py
@@ -3,15 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
+# matplot 라이브러리를 사용하지 않고
+# OpenCV 함수만을 이용해서 히스토그램을 화면에 출력할 수 있다.
 def getGrayHistImage(hist):
-    imgHist = np.full((100, 256), 255, dtype=np.uint8)
+    imgHist = np.full((100, 256), 255, dtype=np.uint8)  # h: 100, w: 256으로 제한.
 
     histMax = np.max(hist)
     for x in range(256):
         pt1 = (x, 100)
-        pt2 = (x, 100 - int(hist[x, 0] * 100 / histMax))
-        cv2.line(imgHist, pt1, pt2, 0)
+        pt2 = (x, 100 - int(hist[x, 0] * 100 / histMax))    # 히스토그램 길이가 100px 위로는 올라가지 않도록 제한.
+        cv2.line(imgHist, pt1, pt2, 0)  # 그리는 함수.
 
     return imgHist
 
